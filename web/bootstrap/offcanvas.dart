a5ade12afb66d0347d963189b28c7daaf68a5b4b
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-12-05 19:48:43 +0000

    bootstrap offcanvas

---
+++ web/bootstrap/offcanvas.dart
@@ -0,0 +1,89 @@
+import 'dart:html' as html;
+import 'package:deact/deact.dart';
+import 'package:deact/deact_html52.dart';
+import 'package:js/js.dart';
+
+import 'bootstrap_core.dart';
+
+enum OffcanvasPlacement {
+  top,
+  bottom,
+  start,
+  end,
+}
+
+extension OffcanvasPlacementExt on OffcanvasPlacement {
+  String get name => toString().split('.').last;
+}
+
+Map<String, Object> offcanvasAttributes({
+  required OffcanvasPlacement placement,
+  String? className,
+  String? id,
+  String? labelledBy,
+  bool scroll = false,
+  bool backdrop = true,
+  bool keyboard = true,
+}) =>
+    {
+      'class':
+          '${className ?? ''} offcanvas offcanvas-${placement.name}',
+      'data-bs-scroll': scroll,
+      'data-bs-backdrop': '$backdrop',
+      'data-bs-keyboard': keyboard,
+      'tabindex': "-1",
+      if (labelledBy != null) 'aria-labelledby': labelledBy,
+      if (id != null) 'id': id,
+    };
+
+/// use [offcanvasAttributes] to populate [attributes]
+DeactNode offcanvas({
+  Object? key,
+  required Map<String, Object> attributes,
+  required Iterable<DeactNode> title,
+  required Iterable<DeactNode> body,
+  String? labelId,
+  bool showCloseButton = true,
+  Ref<Offcanvas?>? offcanvasRef,
+}) {
+  return fc((ctx) {
+    final ref = ctx.ref<html.Element?>('ref', null);
+    ctx.hookEffect(() {
+      final _offcanvas = Offcanvas(ref.value!);
+      if (offcanvasRef != null) offcanvasRef.value = _offcanvas;
+    });
+
+    return el(
+      'div',
+      ref: ref,
+      key: key,
+      attributes: attributes,
+      children: [
+        div(
+          className: 'offcanvas-header',
+          children: [
+            h5(
+              className: 'offcanvas-title',
+              id: labelId,
+              children: title,
+            ),
+            if (showCloseButton)
+              closeButton(attributes: {'data-bs-dismiss': 'offcanvas'}),
+          ],
+        ),
+        div(
+          className: 'offcanvas-body',
+          children: body,
+        ),
+      ],
+    );
+  });
+}
+
+@JS('bootstrap.Offcanvas')
+class Offcanvas {
+  external Offcanvas(html.Element element);
+  external void toggle();
+  external void show();
+  external void hide();
+}
