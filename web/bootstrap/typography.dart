5d9aea917f99ae5f3eacc33f3cc50d4f32e4fdda
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-12-08 15:37:47 +0000

    bootstrap typegraphy

---
+++ web/bootstrap/typography.dart
@@ -0,0 +1,35 @@
+/// https://getbootstrap.com/docs/5.0/content/typography/
+class BTypography {
+  static const h1 = 'h1';
+  static const h2 = 'h2';
+  static const h3 = 'h3';
+  static const h4 = 'h4';
+  static const h5 = 'h5';
+  static const h6 = 'h6';
+
+  static const textMuted = 'text-muted';
+
+  static const display1 = 'display-1';
+  static const display2 = 'display-2';
+  static const display3 = 'display-3';
+  static const display4 = 'display-4';
+  static const display5 = 'display-5';
+  static const display6 = 'display-6';
+
+  static const lead = 'lead';
+
+  static const mark = 'mark';
+  static const del = 'del';
+  static const s = 'text-decoration-line-through';
+  static const ins = 'ins';
+  static const u = 'text-decoration-underline';
+  static const small = 'small';
+  static const strong = 'strong';
+  static const em = 'em';
+
+  static const attribute = 'attribute';
+  static const initialism = 'initialism';
+
+  static const blockquote = 'blockquote';
+  static const blockquoteFooter = 'blockquote-footer';
+}
